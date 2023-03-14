0aec532eec018d1ef1f67ce0d90c19bba199b288
luqman al hakim <luqmanalhakim061@gmail.com>
2023-03-14 20:05:03 +0700

    menambahkan file baru

---
+++ Modul_01/Exercise-01.py
@@ -0,0 +1,15 @@
+nama="Luqman Al Hakim"
+NIM="I0322069"
+Asal="Karanganyar"
+Visualisasi_asal="di tempat saya masih banyak sawah dan perkebunan yang luas"
+Harapan_01="saya menjadi lebih faham bahasa pemrograman"
+Harapan_02="cepat diterima kerja dan dapat mengaplikasikan apa yang saya pelajari di perkuliahan"
+
+print("=====Assalamualaikum wr wb=====")
+print("Perkenalkan nama saya", nama)
+print("NIM saya", NIM)
+print("Saya berasal dari kota", Asal)
+print("sedikit gambaran bahwa", Visualisasi_asal)
+print("Harapan saya setelah mengikuti praktikum programa komputer", Harapan_01)
+print("Harapan saya setelah lulus dari Teknik Industri", Harapan_02)
+print("=====Sekian, Wassalamualaikum wr wb=====")
